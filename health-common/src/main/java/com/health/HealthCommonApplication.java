949d4bc0a3361e6d5ead20b7210228967888f430
xjzh1986 <273904882@163.com>
2020-01-08 11:28:20 +0800

    框架搭建

---
+++ health-common/src/main/java/com/health/HealthCommonApplication.java
@@ -0,0 +1,13 @@
+package com.health;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthCommonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthCommonApplication.class, args);
+	}
+
+}
