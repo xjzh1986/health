5ea2061c0e98ede7f96ab18577e97de6cedd39d2
xjzh1986 <273904882@163.com>
2020-01-08 11:15:56 +0800

    health 框架提交

---
+++ health-sys/src/main/java/com/health/HealthSysApplication.java
@@ -0,0 +1,13 @@
+package com.health;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthSysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthSysApplication.class, args);
+	}
+
+}
