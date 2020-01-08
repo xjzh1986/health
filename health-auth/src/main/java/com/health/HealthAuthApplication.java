3f9c09d6283ab62946cb607437ca665485404dec
xjzh1986 <273904882@163.com>
2020-01-08 11:29:34 +0800

    框架搭建

---
+++ health-auth/src/main/java/com/health/HealthAuthApplication.java
@@ -0,0 +1,13 @@
+package com.health;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthAuthApplication.class, args);
+	}
+
+}
