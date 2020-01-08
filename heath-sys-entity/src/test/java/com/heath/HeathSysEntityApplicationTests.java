8cc34c8fb3b877000c7c4aaa952fe0ca8855e76a
xjzh1986 <273904882@163.com>
2020-01-08 11:17:04 +0800

    health框架提交

---
+++ heath-sys-entity/src/test/java/com/heath/HeathSysEntityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heath;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeathSysEntityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
