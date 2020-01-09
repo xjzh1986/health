ee16d2c090db86367f484b622d11114020a8b589
xjzh1986 <273904882@163.com>
2020-01-09 14:25:12 +0800

    框架提交

--- health-sys/src/main/java/com/health/HealthSysApplication.java
+++ health-sys/src/main/java/com/health/HealthSysApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @MapperScan("com.health.mapper")
 @SpringBootApplication(scanBasePackages = "com.health")
 public class HealthSysApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(HealthSysApplication.class, args);
 	}
