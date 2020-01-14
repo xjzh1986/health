710c108dd54019f18b60c69d83bfc9ddb701606b
xjzh1986 <273904882@163.com>
2020-01-14 13:22:18 +0800

    springcloud 微服务框架提交

--- health-order/src/main/java/com/health/HealthOrderApplication.java
+++ health-order/src/main/java/com/health/HealthOrderApplication.java
@@ -3,7 +3,9 @@ package com.health;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @MapperScan("com.health.mapper")
 @SpringBootApplication(scanBasePackages = "com.health")
 public class HealthOrderApplication {
